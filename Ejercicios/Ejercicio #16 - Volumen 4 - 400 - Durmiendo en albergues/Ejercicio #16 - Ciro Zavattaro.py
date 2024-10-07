a4157b9fa0601eea3bd53c15e40eaca448f9a041
Ciroka <cirozavattarocampos@gmail.com>
2024-10-07 17:09:29 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio #16 - Volumen 4 - 400 - Durmiendo en albergues/Ejercicio #16 - Ciro Zavattaro.py
@@ -0,0 +1,37 @@
+cadena=input()
+inicio=10000000000000
+final=0
+sin_inicio=False
+for i in range(len(cadena)):
+    if cadena[i]=="X" and i<inicio:
+        inicio=i
+    if cadena[i]=="X":
+        final=i
+cont_antes=0
+cont_desp=0
+cont_medio=0
+for x in range (len(cadena)):
+    if x<inicio:
+        cont_antes+=1
+    elif x>final:
+        cont_desp+=1
+    elif x>inicio and x<final:
+        cont_medio+=1
+        if cadena[x]=="X":
+            cont_medio=0
+if cont_medio==0:
+    if cont_desp>cont_antes:
+        print(cont_desp-1)
+    else:
+        print(cont_antes-1)
+else:
+    if cont_medio>cont_antes and cont_medio>cont_desp:
+        if cont_medio%2==0:
+            cont_medio=cont_medio//2-1
+        else:
+            cont_medio//=2
+        print (cont_medio)
+    elif cont_desp>cont_antes and cont_desp>=cont_medio:
+        print(cont_desp-1)
+    else:
+        print(cont_antes-1)
